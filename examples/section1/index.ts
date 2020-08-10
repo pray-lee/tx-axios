3bbebf46f09fdcc2d07cba7e0556149ee96765bb
pray-lee <praycis@gmail.com>
2020-08-10 17:18:23 +0800

    update

--- examples/section1/index.ts
+++ examples/section1/index.ts
@@ -299,3 +299,58 @@ if(demo.fullName) {
    console.log(demo.fullName)
 }
 
+// 静态属性
+class Grid {
+    static origin = {x: 0, y: 0}
+
+    scale: number
+
+    constructor(scale: number) {
+       this.scale = scale
+    }
+
+    caculateDistanceFromOrigin(point: {x: number; y: number}) {
+        let xDist = point.x - Grid.origin.x
+        let yDist = point.y - Grid.origin.y
+        return Math.sqrt(xDist * xDist + yDist * yDist) * this.scale
+    }
+}
+
+let grid1 = new Grid(1)
+let grid2 = new Grid(5)
+
+console.log(grid1.caculateDistanceFromOrigin({x: 3, y: 4}))
+console.log(grid2.caculateDistanceFromOrigin({x: 3, y: 4}))
+
+// 抽象类, 抽象类不可以被实例化, 抽象类中的方法不包含具体实现并且必须在派生类中实现。
+abstract class Department {
+    constructor(public name: string) {
+    }
+
+    printName(): void{
+        console.log('Department name: ' + this.name)
+    }
+
+    abstract printMeeting():void // 必须在派生类中实现
+}
+
+class AccountingDepartment extends Department {
+   constructor() {
+       super('Accounting and Auditing')
+   }
+
+   printMeeting(): void {
+       console.log('The accounting department meets each Monday at 10am.')
+   }
+
+   generateReports(): void {
+      console.log('Generating accounting reports...')
+   }
+}
+
+let department: Department // 允许创建一个对抽象类型的引用
+// department = new Department() // 不能创建抽象类的实例
+department = new AccountingDepartment() // 允许对一个抽象子类进行实例化和赋值
+department.printName()
+department.printMeeting()
+// department.generateRepoorts() // 方法在生命的抽象类中不存在
